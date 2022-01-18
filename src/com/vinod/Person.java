51ec2b90249400b32a64abc2845b0e63f8cffc78
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2022-01-18 14:22:00 +0530

    Update Person.java

--- src/com/vinod/Person.java
+++ src/com/vinod/Person.java
@@ -1,9 +1,11 @@
 package com.vinod;
 
+
 public class Person {
 
     private String name;
     private String email;
     private String phone;
 
+    // TODO: add constructors and setters/getters along with toString
 }
